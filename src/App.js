070bc0d1dae99cedcf5d1c12c64820eb6a1cad16
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-28 15:11:47 +0600

    Added nav component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
-
+import Nav from "./components/Nav/Nav";
 function App() {
+
+  
   return (
     <div className="App">
-
+<Nav/>
     </div>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,29 @@
+import "./Nav.css";
+function Nav() {
+  const rightLinks = ["link3", "link4", "link5"];
+  const leftLinks = ["link1", "link2"];
+
+  const left = leftLinks.map((leftlink) => {
+    return (
+      <li>
+        <a href="#">{leftlink}</a>
+      </li>
+    )
+  })
+  const right = rightLinks.map((rightlink) => {
+    return (
+      <li>
+        <a href="#">{rightlink}</a>
+      </li>
+    )
+  })
+
+return (
+<div className="Ul">
+  <ul>{left} {right}</ul>
+ 
+</div>
+)
+}
+
+export default Nav;
